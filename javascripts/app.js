3a62ad4c1482e99c29def05d105b901c39453a43
Gutu Galuppo <galuppodev@gmail.com>
2018-10-07 21:05:17 +0200

    First commit

---
+++ javascripts/app.js
@@ -0,0 +1,15 @@
+var rover = "Robert";
+rover.direction = "N";
+
+
+function turnLeft(rover){
+  console.log("turnLeft was called!");
+}if ()
+
+function turnRight(rover){
+  console.log("turnRight was called!");
+}
+
+function moveForward(rover){
+  console.log("moveForward was called")
+}
